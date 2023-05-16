24ff101d6522f47123ec27ecafb76ab51f2dca1c
qorals1121 <qorals1121@naver.com>
2023-05-16 22:41:51 +0900

    ~useState의 함수형 업데이트 까지

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import {
     MdCheckBoxOutlineBlank,
     MdCheckBox,
@@ -23,4 +24,4 @@ const TodoListItem = ({ todo, onRemove, onToggle }) => {
     );
 };
 
-export default TodoListItem;
+export default React.memo(TodoListItem);
